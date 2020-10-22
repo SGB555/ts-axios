c007d9547d6403d655097376607e460c3f5040a1
沈广博 <654351355@qq.com>
2020-10-22 14:39:50 +0800

    chapter2-处理header

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -70,6 +70,23 @@ axios({
   }
 })
 
-const arr = new Int32Array([21, 31])
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
 
-axios({ method: 'post', url: '/base/buffer', data: arr })
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
+})
